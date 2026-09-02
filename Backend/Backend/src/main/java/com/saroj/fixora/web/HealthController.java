dc73f0cfbffdf897aea186b95e7dcbe905735db9
Mr-Saroj <sarojaojha09@gmail.com>
2026-09-02 12:14:54 +0530

    Adding Helth router

---
+++ Backend/Backend/src/main/java/com/saroj/fixora/web/HealthController.java
@@ -0,0 +1,13 @@
+package com.saroj.fixora.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Fixora Backend is Running!";
+    }
+}
